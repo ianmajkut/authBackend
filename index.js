de64a28589aad43d87b4681e093b01b3ab3163ca
ianmajkut <ianedwinmajkut@gmail.com>
2022-01-14 17:44:55 -0300

    feat: conectar Mongo Atlas DB y crear modelo DB
    Para conectar con Mongo Atlas usamos mongoose y se creo una funcion asincrona para poder usar el await al conectar con la base de datos. En caso de algun error, tirara una excepcion. Tambien creamos un schema para los usuarios teniendo el name, email y password como propiedades y finalmente se crea el modelo con model()

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const { dbConnection } = require('./db/config')
 require('dotenv').config()
 
 //Crear el servidor express
 const app = express()
+
+//DB
+dbConnection()
 
 //Directorio Público
 app.use(express.static('public'))
