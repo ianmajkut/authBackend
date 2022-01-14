93577b5ef08a906127bbf2f0d530c29a1cb5510f
ianmajkut <ianedwinmajkut@gmail.com>
2022-01-14 18:07:17 -0300

    feat: crear usuario, encriptar password y subir a DB
    El metodo crearUsuario lo hicimos async para poder usar el await a lo largo de el metodo. Primero buscamos en la db si hay un mail que se similar al que obtenemos de la request. En ese caso, tiramos un error. De lo contrario creamos un Usario usando el model de la db, despues hasheamos la contraseña y guardamos el user en la DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,53 @@
 const {response} = require('express')
+const Usuario = require('../models/Usuario')
+const bcrypt = require('bcryptjs')
 
-const crearUsuario = (req, res = response)=>{
+const crearUsuario = async (req, res = response)=>{
 
           const {name, email, password} = req.body
 
-          return res.json({
-                    ok: true,
-                    msg: 'Crear nuevo usuario',
-          })
+          try{
+                    // Verificar emal
+                    const usuario = await Usuario.findOne({email: email})
+                    if(usuario){
+                              return res.status(400).json({
+                                        ok: false,
+                                        msg: 'El usuario ya existe con ese email',
+                              })
+                    }         
+
+                    // Crear usuario con el modelo
+                    
+                    const dbUser = new Usuario(req.body)
+
+                    // Hacer un hash a la password
+
+                    const salt = bcrypt.genSaltSync()
+                    dbUser.password = bcrypt.hashSync(password, salt)
+
+                    // Generar el JWT 
+
+                    // Crear el usuario de DB
+
+                    await dbUser.save()
+
+                    // Generar respuesta 
+                    
+                    return res.status(201).json({
+                              ok: true,
+                              uid: dbUser.id,
+                              name: name
+                    })
+
+          }catch(error){
+                    console.log(error)
+                    return res.status(500).json({
+                              ok: false,
+                              msg: 'Por favor hable con el administrador',
+                    })
+          }
+
+          
 }
 
 const loginUsuario = (req, res = response)=>{
